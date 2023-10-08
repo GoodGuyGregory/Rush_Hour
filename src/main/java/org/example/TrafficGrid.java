b453f41f6b5d58b6070ebb13b20176b39595370f
GoodGuyGregory <greg.witt625@gmail.com>
2023-10-08 16:24:33 -0700

    initial commit for the project

---
+++ src/main/java/org/example/TrafficGrid.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.lang.reflect.Array;
+
+public class TrafficGrid {
+
+    // create a reference to the currentState
+    private Array[][] currentState;
+    private List<Car> carPositions;
+//    private previousStates
+    private
+
+
+}
