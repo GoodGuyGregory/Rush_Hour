7c105e181540735c53777c876d33ba1ab0460fe7
Greg Witt <greg.witt@avnet.com>
2023-10-11 23:57:42 -0400

    adds locateBiker and locateCar actions

--- src/main/java/org/example/TrafficGrid.java
+++ src/main/java/org/example/TrafficGrid.java
@@ -8,7 +8,11 @@ public class TrafficGrid {
     // create a reference to the currentState
     private int parkingLotWidth;
     private int parkingLotHeight;
+    // holds goal state 
+    private Biker biker;
     private char[][] currentState;
+    private List<Car> idleCars;
+    private int movesCounter;
 
     public TrafficGrid() {}
 
@@ -17,6 +21,7 @@ public class TrafficGrid {
         this.parkingLotHeight = parkingLotHeight;
         this.currentState = currentState;
     }
+
 
     public int getParkingLotWidth() {
         return parkingLotWidth;
@@ -38,6 +43,65 @@ public class TrafficGrid {
 
     public void setCurrentState(char[][] currentState) {
         this.currentState = currentState;
+    }
+
+    public void printTrafficGrid() {
+        for (int i = 0; i < currentState.length; i++) {
+            for (int j = 0; j < currentState.length; j++) {
+                System.out.print(currentState[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    // locates the Biker item
+    // locates the goalstate of the biker
+    public void locateBiker() {
+            int[] coordinates = new int[2];
+            for (int i = 0; i < currentState.length; i++) {
+                for (int j = 0; j < currentState.length; j++) {
+                    if (currentState[i][j] == '>') {
+                        coordinates[0] = i;
+                        coordinates[1] = j;
+                        this.biker.setRowPosition(i);
+                        this.biker.setColPosition(j);
+                        int[] goalPos = new int[2];
+                        goalPos[0] = i;
+                        goalPos[1] = currentState.length;
+                        this.biker.setGoalPosition(goalPos);
+                }
+                
+            }
+        }
+    }
+
+    // locates cars and establishes their attributes.
+    // adds them to a list within the TrafficGrid
+        public void locateCars() {
+            for (int i = 0; i < currentState.length; i++) {
+                for (int j = 0; j < currentState.length; j++) {
+        
+                    if (currentState[i][j] == '-') {
+                        Car foundCar = new Car(Orientation.Horizontal,i,j);
+                        this.idleCars.add(foundCar);
+                    }
+                    
+                    if (currentState[i][j] == '|') {
+                        Car foundCar = new Car(Orientation.Vertical,i,j);
+                        this.idleCars.add(foundCar);
+                    }
+                
+            }
+        }
+    }
+
+        // establishes car counts and their positions
+        // establishes biker attributes and goal state 
+    public void buildParkingLot(){
+
+        locateBiker();
+        locateCars();
+    
     }
 
     public void moveCars() {}
